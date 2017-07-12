6f76ad48a63dc804300def2e8a78405d10465267
Christian Zufferey <christian@zufferey.com>
2017-07-12 17:18:21 +0200

    - ajout du script python t1.py qui envoie un résultat en HTML 1.1 - ajout du fichier index.html pour pointer sur t1.py - ajout du script start.sh pour démarrer le serveur web

---
+++ cgi-bin/t1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Content-Type: text/html;\n\n")
+print("hello zuzu<br>")
+print("2x3= " + str(2*3))
+
+
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#Petit script pour juste tester un petit truc en HTML, démarre simplement un serveur web minimaliste en python
+#zf170712.1145
+
+echo -e "
+
+Afin de garder le petit serveur WEB permanent, il serait bien de le faire tourner dans un 'screen' avec:
+screen -S nanoweb       pour entrer dans screen
+./web_server.sh         pour lancer le serveur WEB dans screen
+CTRL+a d                pour sortir de screen en laissant tourner le serveur
+screen -r nanoweb       pour revenir dans screen
+CTRL+d                  pour terminer screen
+screen -list            pour lister tous les screen en fonctionement
+
+votre adresse IP:
+"
+
+ifconfig | grep "inet ad" | cut -f2 -d: | awk '{print $1}'
+
+python3 -m http.server 18080 --cgi
+
+
